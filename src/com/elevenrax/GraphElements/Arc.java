cf402dce821db1fe8443bfa80317c69449c920a6
Nate <nate@11rax.com>
2017-12-18 18:46:11 +1100

    publish to github

---
+++ src/com/elevenrax/GraphElements/Arc.java
@@ -0,0 +1,36 @@
+package com.elevenrax.GraphElements;
+
+
+public class Arc {
+
+    private Node mNext;
+    private int mWeight;
+
+
+    public Arc(Node next, int weight) {
+        mNext = next;
+        mWeight = weight;
+    }
+
+
+    public Node getDestinationNode() {
+        return mNext;
+    }
+
+
+    public int getWeight() {
+        return mWeight;
+    }
+
+
+    public void setWeight(int weight) {
+        mWeight = weight;
+    }
+
+
+    @Override
+    public String toString() {
+        return "[" + mNext.getId() + "]" + " w(" + mWeight + ") ";
+    }
+
+}
